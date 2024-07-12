d281354e813a2526d8df28508b151e2a960c6b41
Saeedinum <mo7amed0ptr@gmail.com>
2024-07-12 10:34:40 +0300

    handle loading state with react skeleton

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,6 +5,7 @@ import {useDispatch} from "react-redux";
 import {addToCart} from "../rtk/cartSlice";
 
 import {useGetProductsQuery} from "../rtk/productsAPI";
+import ProductsSkeleton from "../skeleton/ProductsSkeleton";
 
 export default function Products() {
 	const dispatch = useDispatch();
@@ -13,48 +14,57 @@ export default function Products() {
 	return (
 		<Container style={{paddingTop: "60px"}}>
 			<Row style={{display: "flex", flexWrap: "wrap"}}>
-				{products?.map((product) => (
-					<Col key={product.id}>
-						<Card
-							style={{
-								width: "18rem",
-								height: "500px",
-								backgroundColor: "rgb(233 229 229)",
-								margin: "auto",
-							}}
-						>
-							<Card.Img
-								style={{
-									height: "300px",
-									width: "90%",
-									margin: "auto",
-									paddingTop: "10px",
-								}}
-								variant='top'
-								src={product.image}
-							/>
-							<Card.Body>
-								<Card.Title>{product.title}</Card.Title>
-								<Card.Text style={{padding: "10px"}}>Price : {product.price}$</Card.Text>
-								<Button
-									style={{
-										position: "absolute",
-										bottom: "5px",
-										right: "85px",
-										backgroundColor: "#9239e5",
-										border: "none",
-									}}
-									variant='primary'
-									onClick={() => {
-										dispatch(addToCart(product));
-									}}
-								>
-									Add To Card
-								</Button>
-							</Card.Body>
-						</Card>
+				{isLoading ? (
+					<Col>
+						<ProductsSkeleton />
+						<ProductsSkeleton />
+						<ProductsSkeleton />
+						<ProductsSkeleton />
 					</Col>
-				))}
+				) : (
+					products?.map((product) => (
+						<Col key={product.id}>
+							<Card
+								style={{
+									width: "18rem",
+									height: "500px",
+									backgroundColor: "rgb(233 229 229)",
+									margin: "auto",
+								}}
+							>
+								<Card.Img
+									style={{
+										height: "300px",
+										width: "90%",
+										margin: "auto",
+										paddingTop: "10px",
+									}}
+									variant='top'
+									src={product.image}
+								/>
+								<Card.Body>
+									<Card.Title>{product.title}</Card.Title>
+									<Card.Text style={{padding: "10px"}}>Price : {product.price}$</Card.Text>
+									<Button
+										style={{
+											position: "absolute",
+											bottom: "5px",
+											right: "85px",
+											backgroundColor: "#9239e5",
+											border: "none",
+										}}
+										variant='primary'
+										onClick={() => {
+											dispatch(addToCart(product));
+										}}
+									>
+										Add To Card
+									</Button>
+								</Card.Body>
+							</Card>
+						</Col>
+					))
+				)}
 			</Row>
 		</Container>
 	);
