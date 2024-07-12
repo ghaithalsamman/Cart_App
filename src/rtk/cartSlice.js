d6fe72ef1edfcccd478e77b05dcdc9bed433d645
Saeedinum <mo7amed0ptr@gmail.com>
2024-07-12 10:35:06 +0300

    clear cart with initial state

--- src/rtk/cartSlice.js
+++ src/rtk/cartSlice.js
@@ -1,27 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = [];
 
 const cartSlice = createSlice({
-  initialState: [],
-  name: "cartSlice",
-  reducers: {
-    addToCart: (state, action) => {
-      const findProduct = state.find(
-        (product) => product.id === action.payload.id
-      );
-      if (findProduct) {
-        findProduct.quantity += 1;
-      } else {
-        const productClone = { ...action.payload, quantity: 1 };
-        state.push(productClone);
-      }
-    },
-    deleteFromCart: (state, action) => {
-      return state.filter((product) => product.id !== action.payload.id);
-    },
-    clear: (state, action) => {
-      return [];
-    },
-  },
+	initialState,
+	name: "cartSlice",
+	reducers: {
+		addToCart: (state, action) => {
+			const findProduct = state.find((product) => product.id === action.payload.id);
+			if (findProduct) {
+				findProduct.quantity += 1;
+			} else {
+				const productClone = {...action.payload, quantity: 1};
+				state.push(productClone);
+			}
+		},
+		deleteFromCart: (state, action) => {
+			return state.filter((product) => product.id !== action.payload.id);
+		},
+		clear: (state, action) => initialState,
+	},
 });
-export const { addToCart, deleteFromCart, clear } = cartSlice.actions;
+export const {addToCart, deleteFromCart, clear} = cartSlice.actions;
 export default cartSlice.reducer;
